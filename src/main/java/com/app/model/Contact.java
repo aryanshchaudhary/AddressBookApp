1e1d0f8335b8a9ca2f8b7eaaf0991528d31be0c8
Aryansh Chaudhary <abhichaudhary012002@gmail.com>
2026-03-13 13:38:51 +0900

    [Aryansh Chaudhary] add . UC1 : Create Contact

---
+++ src/main/java/com/app/model/Contact.java
@@ -0,0 +1,10 @@
+package com.app.model;
+import java.util.*;
+public class Contact {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter value : ");
+		String input = sc.nextLine();
+		sc.close();
+	}
+}
